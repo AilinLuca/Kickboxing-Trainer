e75c9a372574efb457d1b001cd4bb5c434ba3b52
Ailin <Ailin@ibm.com>
2020-12-30 22:25:34 -0500

    First commit

---
+++ src/ExerciseCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ExerciseCard() {
+  return (
+    <div>
+      <h1>{/*Exercise Name here*/}</h1>
+      <img src="" alt={/*Exercise image description here*/}>
+        {/*Exercise Image here*/}
+      </img>
+      <h1>{/*Timer here*/}</h1>
+    </div>
+  );
+}
